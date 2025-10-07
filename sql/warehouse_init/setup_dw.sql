4c580df9f18244a3894ac6fc583df3bf41c28be4
Aki <akibukzwork@gmail.com>
2025-10-08 02:38:22 +0800

    ADD: Base Schema

---
+++ sql/warehouse_init/setup_dw.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE finance_dw;
+
+-- Fact Tables
+CREATE TABLE FactTrans;
+CREATE TABLE FactLoan;
+CREATE TABLE FactOrder;
+
+-- Dimension Tables
+CREATE TABLE DimClientAccount;
+CREATE TABLE DimDistrict;
+CREATE TABLE DimDate;
+CREATE TABLE DimCard;
