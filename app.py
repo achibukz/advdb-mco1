0c7e9f313905310c97c72c2ae96787a9f4735c5f
Keisha Cu <keisha.cu050@gmail.com>
2025-10-16 00:15:55 +0800

    style: style changes to web app

--- app.py
+++ app.py
@@ -8,15 +8,17 @@ st.sidebar.header("Choose data to view:")
 report_category = st.sidebar.selectbox("Select Report to View:", ["Loans by Region", "Orders by Bank", "Transactions by Operation"])
 filter_option = st.sidebar.selectbox("Filter by:", ["None", "Region", "Bank", "Operation"])
 
+st.sidebar.markdown("---")  # Adds a horizontal line for separation
+st.sidebar.markdown("Balcita, Bukuhan, Cu, Dimaunahan")
+st.sidebar.markdown("STADVDB S17 | Group 12")
+
 # Placeholder for the main report
-st.write("### Category of Reports")
-st.write("#### Specific Report")
 st.write("Report will be displayed here.")
 
 def get_db_connection():
     return mysql.connector.connect(
-        host="localhost",  # Use "localhost" only
-        port=3307,         # Specify the port separately
+        host="localhost", 
+        port=3307,         
         user="warehouse_user",
         password="rootpass",
         database="warehouse_db"
